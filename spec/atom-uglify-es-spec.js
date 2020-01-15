c9a0839f745862e82a3ba5bdb63f3f6fec20a181
SavageCore <talk@savagecore.eu>
2020-01-15 18:28:11 +0000

    Add tests

--- spec/atom-uglify-es-spec.js
+++ spec/atom-uglify-es-spec.js
@@ -11,6 +11,12 @@ describe('AtomUglifyEs', () => {
 	let editor;
 
 	beforeEach(() => {
+		atom.config.set('atom-uglify-es.compress', true);
+		atom.config.set('atom-uglify-es.mangle', true);
+		atom.config.set('atom-uglify-es.uglifyToFile', false);
+		atom.config.set('atom-uglify-es.onSave', false);
+		atom.config.set('atom-uglify-es.onSaveIfExists', false);
+
 		workspaceElement = atom.views.getView(atom.workspace);
 		activationPromise = atom.packages.activatePackage('atom-uglify-es');
 		openPromise = atom.packages.activatePackage('atom-uglify-es').then(() => {
@@ -22,12 +28,8 @@ describe('AtomUglifyEs', () => {
 		});
 	});
 
-	describe('when the atom-uglify-es:uglify event is triggered', () => {
+	describe('when atom starts', () => {
 		it('activates the plugin', () => {
-			expect(atom.packages.isPackageActive('atom-uglify-es')).toBe(false);
-
-			atom.commands.dispatch(workspaceElement, 'atom-uglify-es:uglify');
-
 			waitsForPromise(() => {
 				return activationPromise;
 			});
@@ -36,7 +38,9 @@ describe('AtomUglifyEs', () => {
 				expect(atom.packages.isPackageActive('atom-uglify-es')).toBe(true);
 			});
 		});
+	});
 
+	describe('when the atom-uglify-es:uglify event is triggered', () => {
 		it('uglifies code in buffer', () => {
 			atom.commands.dispatch(workspaceElement, 'atom-uglify-es:uglify');
 
@@ -72,7 +76,6 @@ describe('AtomUglifyEs', () => {
 
 		it('uglifies code to file', () => {
 			atom.config.set('atom-uglify-es.uglifyToFile', true);
-			atom.commands.dispatch(workspaceElement, 'atom-uglify-es:uglify');
 
 			waitsForPromise(() => {
 				return openPromise;
@@ -90,7 +93,95 @@ describe('AtomUglifyEs', () => {
 					expect(data).toBe('async function test(){await fetch("http://example.com")}');
 					fs.unlinkSync(minSamplePath);
 				});
+
 				atom.commands.dispatch(workspaceElement, 'atom-uglify-es:uglify');
+			});
+		});
+	});
+
+	describe('when saving', () => {
+		it('uglifies code to file when onSave enabled', () => {
+			atom.config.set('atom-uglify-es.uglifyToFile', true);
+			atom.config.set('atom-uglify-es.onSave', true);
+
+			waitsForPromise(() => {
+				return openPromise;
+			});
+
+			runs(() => {
+				const minSamplePath = path.dirname(editor.getPath()) + '\\sample.min.js';
+				const watcher = chokidar.watch(minSamplePath, {
+					persistent: true
+				});
+
+				editor.setText('async function test() {\n\tconst data = await fetch(\'http://example.com\');\n}');
+				watcher.on('add', () => {
+					const data = fs.readFileSync(minSamplePath, {encoding: 'utf-8'});
+					expect(data).toBe('async function test(){await fetch("http://example.com")}');
+					fs.unlinkSync(minSamplePath);
+				});
+				editor.save();
+			});
+		});
+
+		it('does not uglify code to file when onSave disabled', () => {
+			atom.config.set('atom-uglify-es.uglifyToFile', true);
+			atom.config.set('atom-uglify-es.onSave', false);
+
+			waitsForPromise(() => {
+				return openPromise;
+			});
+
+			runs(() => {
+				const minSamplePath = path.dirname(editor.getPath()) + '\\sample.min.js';
+
+				editor.setText('async function test() {\n\tconst data = await fetch(\'http://example.com\');\n}');
+				editor.save();
+				fs.access(minSamplePath, fs.constants.F_OK, err => {
+					expect(err.errno).toBe(-2);
+				});
+			});
+		});
+
+		it('uglifies code to file when uglified file exists and onSaveIfExists enabled', () => {
+			atom.config.set('atom-uglify-es.uglifyToFile', true);
+			atom.config.set('atom-uglify-es.onSave', true);
+			atom.config.set('atom-uglify-es.onSaveIfExists', true);
+
+			waitsForPromise(() => {
+				return openPromise;
+			});
+
+			runs(() => {
+				const minSamplePath = path.dirname(editor.getPath()) + '\\sample.min.js';
+				fs.closeSync(fs.openSync(minSamplePath, 'w'));
+
+				editor.setText('async function test() {\n\tconst data = await fetch(\'http://example.com\');\n}');
+				editor.save();
+				fs.access(minSamplePath, fs.constants.F_OK, err => {
+					expect(err.errno).toBe(-2);
+				});
+				fs.unlinkSync(minSamplePath);
+			});
+		});
+
+		it('does not uglify code to file when uglified file does not exist and onSaveIfExists enabled)', () => {
+			atom.config.set('atom-uglify-es.uglifyToFile', true);
+			atom.config.set('atom-uglify-es.onSave', true);
+			atom.config.set('atom-uglify-es.onSaveIfExists', true);
+
+			waitsForPromise(() => {
+				return openPromise;
+			});
+
+			runs(() => {
+				const minSamplePath = path.dirname(editor.getPath()) + '\\sample.min.js';
+
+				editor.setText('async function test() {\n\tconst data = await fetch(\'http://example.com\');\n}');
+				editor.save();
+				fs.access(minSamplePath, fs.constants.F_OK, err => {
+					expect(err.errno).toBe(-2);
+				});
 			});
 		});
 	});
